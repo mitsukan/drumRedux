a2780d8b3d6f5b5eed261f06adcf42e7fe71f274
Jihin Ip <jim_ip20@hotmail.com>
2019-03-23 18:32:56 +0000

    Third tick confirmed

--- tempo.test.js
+++ tempo.test.js
@@ -12,12 +12,14 @@ describe("tempo", () => {
   it("can change the state in a score", () => {
     tempo.play(callback);
     expect(callback).not.toBeCalled();
-    jest.advanceTimersByTime(501);
+    jest.advanceTimersByTime(500);
     expect(tempo.score).toEqual([true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]);
     expect(setInterval).toHaveBeenCalledTimes(1);
     expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 500);
     jest.advanceTimersByTime(500);
     expect(tempo.score).toEqual([false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false]);
+    jest.advanceTimersByTime(500);
+    expect(tempo.score).toEqual([false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false]);
   });
 
 });
